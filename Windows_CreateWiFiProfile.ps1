7482968e4b7f3df0956e2b33d8788069fe482f76
Slaine1987 <gert-jan@fam-teune.nl>
2022-10-03 22:53:48 +0200

    Create Windows_CreateWiFiProfile.ps1

---
+++ Windows_CreateWiFiProfile.ps1
@@ -0,0 +1,37 @@
+﻿$ExportFile="WifiProfile.xml"
+$SSID="SSIDNAME"
+$Secret="Secret"
+
+$SSIDHEX=($SSID.ToCharArray() |foreach-object {'{0:X}' -f ([int]$_)}) -join''
+$xmlfile="<?xml version=""1.0""?>
+<WLANProfile xmlns=""http://www.microsoft.com/networking/WLAN/profile/v1"">
+    <name>$SSID</name>
+    <SSIDConfig>
+        <SSID>
+            <hex>$SSIDHEX</hex>
+            <name>$SSID</name>
+        </SSID>
+    </SSIDConfig>
+    <connectionType>ESS</connectionType>
+    <connectionMode>auto</connectionMode>
+    <MSM>
+        <security>
+            <authEncryption>
+                <authentication>WPA2PSK</authentication>
+                <encryption>AES</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+            <sharedKey>
+                <keyType>passPhrase</keyType>
+                <protected>false</protected>
+                <keyMaterial>$Secret</keyMaterial>
+            </sharedKey>
+        </security>
+    </MSM>
+</WLANProfile>
+"
+
+$XMLFILE > ($ExportFile)
+netsh wlan add profile filename="$($ExportFile)"
+netsh wlan show profiles $SSID key=clear
+netsh wlan connect name=$SSID
